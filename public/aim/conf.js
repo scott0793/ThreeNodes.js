b9ecbda049f7ffb1a5bfe818dbe1d755423fe285
Scot <scot@ubuntu.ubuntu-domain>
2012-02-24 12:11:33 +0100

    added public/aim/conf.js file for server configuration, be sure to change the corresponding /etc/hosts file

--- public/aim/conf.js
+++ public/aim/conf.js
@@ -1,8 +1,8 @@
 define(function () {
   return {
-	  server_name : 'local.host'
+	  server_name : 'totally.weird.servername.com'
 	, port_id : '8042'
-	, full_server_name : 'http://' + 'local.host' + ':' + '8042'	
+	, full_server_name : 'http://' + 'totally.weird.servername.com' + ':' + '8042'	
   }
 });
 
