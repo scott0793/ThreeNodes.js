348eb3601452b5773aad458c1436335e21ec720f
Scot <scot@ubuntu.ubuntu-domain>
2012-02-28 15:01:55 +0100

    change conf.js to almendedemo.customers.luna.net:8484

--- public/aim/conf.js
+++ public/aim/conf.js
@@ -1,8 +1,8 @@
 define(function () {
   return {
-	  server_name : 'almendedemo1.customers.luna.net'
+	  server_name : 'almendedemo.customers.luna.net'
 	, port_id : '8042'
-	, full_server_name : 'http://' + 'almendedemo1.customers.luna.net' + ':' + '8042'	
+	, full_server_name : 'http://' + 'almendedemo.customers.luna.net' + ':' + '8484'	
   }
 });
 
